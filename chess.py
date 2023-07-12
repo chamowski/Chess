daa985dc12901c018955bd7ee751635752791096
chamowski <chamowski@hotmail.com>
2023-07-12 10:11:59 +1000

    latest wed morning

--- chess.py
+++ chess.py
@@ -5,4 +5,130 @@ Created on Tue Jul 11 13:37:14 2023
 @author: chamo
 """
 
-added something
+# Welcome message and basic dimensions of board
+print("")
+print("Welcome to my chess game!")
+print("")
+
+board_dim = 8
+def init_board():
+   
+    board = [["\u2610" for j in range(8)]for i in range(8)]
+    return board
+
+board = init_board()    
+#print(board)
+
+# Set the starting position of all of the pieces on the board
+
+# black pieces
+board[0][0]="\u2656"
+board[0][7]="\u2656"
+board[0][1]="\u265B"
+board[0][6]="\u265B"
+board[0][2]="\u2657"
+board[0][5]="\u2657"
+board[0][3]="\u2655"
+board[0][4]="\u2654"
+for x in range(8):
+   board[1][x] = "\u2659"
+
+# white pieces
+board[7][0]="\u265C"
+board[7][7]="\u265C"
+board[7][1]="\u265E"
+board[7][6]="\u265E"
+board[7][2]="\u265D"
+board[7][5]="\u265D"
+board[7][3]="\u265B"
+board[7][4]="\u265A"
+for x in range(8):
+   board[6][x] = "\u265F"
+
+#print(board)
+
+# This section prints the board in a neat way
+
+rank = [8,7,6,5,4,3,2,1]
+file = ["a","b","c","d","e","f","g","h"]
+def print_board(board):
+   
+    print("  ", end= "")
+    for c in file:
+        print(c+" ", end = " ")
+    print()
+    for i in range(len(board)):
+        print((8-i), end = " ")
+        for j in range(len(board[i])):
+            print(board[i][j], end = " ")
+        print()
+
+print_board(board)
+
+# This next section asks the player to input the square 
+# moved from and the square moved to
+
+print("")
+mov_from = str(input("Which rank and file are you moving from in that order?: (eg: 2d): "))
+print("")
+mov_to = str(input("Which rank and file are you moving to?: (eg: 3d): "))
+print("")
+
+# This section separates the rank and file for each of the above inputs
+
+def move_from():
+    from_rank_file = []
+    for i in mov_from:
+        from_rank_file += [i]
+        
+    return from_rank_file
+
+from_rank_file = move_from() 
+
+rank_from = from_rank_file[0]
+file_from = from_rank_file[1]
+
+def move_to():
+    to_rank_file = []
+    for i in mov_to:
+        to_rank_file += [i]
+    
+    return to_rank_file
+
+to_rank_file = move_to() 
+ 
+rank_to = to_rank_file[0]
+file_to = to_rank_file[1]
+
+
+# Transform the human user input to grid coordinates that python understands
+
+row_from = 8 - int(rank_from)
+col_from = file.index(file_from)
+row_to = 8 - int(rank_to)
+col_to = file.index(file_to)
+
+piece = board[row_from][col_from]
+
+# Tell the user what piece they are moving and where to
+
+print(f"You are moving {board[row_from][col_from]} from {mov_from} to {mov_to}")
+print("")
+board[row_from][col_from] = "\u2610"
+board[row_to][col_to] = piece
+
+#Print the new board with the moved piece
+
+def print_board(board):
+   
+    print("  ", end= "")
+    for c in file:
+        print(c+" ", end = " ")
+    print()
+    for i in range(len(board)):
+        print((8-i), end = " ")
+        for j in range(len(board[i])):
+            print(board[i][j], end = " ")
+        print()
+
+print_board(board)
